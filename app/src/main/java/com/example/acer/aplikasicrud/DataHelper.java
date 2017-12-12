0d102a5b7022d6a5f4e476588e24f296e3676075
pramitanisa <pramitanisaaa@gmail.com>
2017-12-12 17:21:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/acer/aplikasicrud/DataHelper.java
@@ -0,0 +1,32 @@
+package com.example.acer.aplikasicrud;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by acer on 11/12/2017.
+ */
+
+public class DataHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "biodatadiri.db";
+    private static final int DATABASE_VERSION = 1;
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // TODO Auto-generated constructor stub
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        String sql = "create table biodata(no integer primary key, nama text null, tgl text null, jk text null, alamat text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+        sql = "INSERT INTO biodata (no, nama, tgl, jk, alamat) VALUES ('1', 'Pramita', '1996-04-01', 'Perempuan','Bandung'), ('2', 'Leni', '1996-08-23', 'Perempuan','Sukabumi'), ('3', 'Nanda', '1996-09-08', 'Laki-laki','Bekasi') ;";
+        db.execSQL(sql);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        // TODO Auto-generated method stub
+    }
+}
